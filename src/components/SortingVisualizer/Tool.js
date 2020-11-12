c86c168b5a1c09d7091b1ec3029e064d678c89dd
sepehr ataei <s2ataei@ryerson.ca>
2020-11-11 20:01:05 -0500

    implemented 2D Conv layer algorithm (4 Loop) and random initializations

--- src/components/SortingVisualizer/Tool.js
+++ src/components/SortingVisualizer/Tool.js
@@ -7,12 +7,15 @@ class SortingVisualizer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            input: [[]]
+            input: [[]],
+            kernel: [[]],
+            output: [[]]
 
         };
         
         this.generateInput = this.generateInput.bind(this);
         this.generateOutput = this.generateOutput.bind(this);
+        this.generateKernel = this.generateKernel.bind(this);
     }
 
     generateInput(event) {
@@ -22,23 +25,61 @@ class SortingVisualizer extends React.Component {
         const input = [];
         while(randomValues.length) input.push(randomValues.splice(0,6));
 
-        console.log(randomValues)
-
         this.setState({
             input
         });
     }
 
-    generateOutput(event) {
+    generateKernel(event) {
         event.preventDefault();
+
+        const randomValues = Array.from({length: 9}, () => Math.random().toFixed(1));
+        const kernel = [];
+        while(randomValues.length) kernel.push(randomValues.splice(0,3));
+
+        this.setState({
+            kernel
+        })
 
     }
 
-    render () {
+    generateOutput(event) {
+        event.preventDefault();
 
-        var kernel = [[1,2,3],[1,2,3],[1,2,3]];
+        const hi = this.state.input[0].length;
+        const wi = this.state.input[1].length;
+        const hk = this.state.kernel[0].length;
+        const wk = this.state.kernel[1].length;
+
+        const paddedInput = Array(8).fill(0).map(x => Array(8).fill(0))
+        const output = Array(6).fill(0).map(x => Array(6).fill(0))
+
+        for (let i = 0; i < hi; i++) {
+            for (let j = 0; j < wi; j++) {
+                paddedInput[i+1][j+1] = this.state.input[i][j]
+            }
+        }
+    
+        for (let i = 0; i < hi; i++) {
+            for (let j = 0; j < wi; j++) {
+                for (let k = 0; k < hk; k++) {
+                    for (let l = 0; l < wk; l++) {
+                        console.log(output)
+                        output[i][j] += paddedInput[i+k][j+l]*this.state.kernel[k][l];
+                       
+                    }
+                }
+            }
+        }
+
+        this.setState({
+            output
+        })
+    }
+
+    render () {
         return (
-            <div>
+            <div className="full-width">
                 <div className="code">
                     <pre className="code"> 
                         <code>
@@ -60,10 +101,36 @@ image = np.random.rand(6,6)`}
 {`class MyModel(tf.keras.Model):
     def __init__(self):
         super(MyModel, self).__init__()
-        self.conv1 = Conv2D(32, 3, activation='relu')
-
-    def call(self, x):
-        return self.conv1(x)
+        initializer = tf.keras.initializers.RandomUniform(minval=0., maxval=1.)
+        self.conv1 = Conv2D(filters=1,
+                            kernel_size=3, 
+                            strides=(1,1), 
+                            activation=none,
+                            use_bias=False, 
+                            kernel_initializer=initializer)`}
+                        </code>
+                    </pre>
+                    <button className="submit add" onClick={this.generateKernel}>Run</button>
+                </div>
+                <div className="flex">
+                    <div className="input">
+                        {this.state.input.map(item => 
+                        <div className="rows">
+                            {item.map(item => <div className="columns">{item}</div> )}
+                        </div>)}
+                    </div>
+                    <div className="kernel">
+                        {this.state.kernel.map(items =>
+                        <div className="rows">
+                            {items.map(item => <div className="columns">{item}</div>)}
+                        </div>)}
+                    </div>
+                </div>
+                <div className="code">
+                    <pre className="code"> 
+                        <code>              
+{`  def call(self, x):
+        x = self.conv1(x)
 
 model = MyModel()
 
@@ -72,10 +139,12 @@ output = model(image)`}
                     </pre>
                     <button className="submit add" onClick={this.generateOutput}>Run</button>
                 </div>
-                {kernel.map(items =>
-                <div className="rows">
-                    {items.map(item => <div className="columns">{item}</div>)}
-                </div>)}
+                <div className="output">
+                    {this.state.output.map(items =>
+                    <div className="rows">
+                        {items.map(item => <div className="columns">{item.toFixed(1)}</div>)}
+                    </div>)}
+                </div>
             </div>
             
         
