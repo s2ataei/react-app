367f5afaa1f7242916572b5ed748f78d10dce286
sepehr ataei <s2ataei@ryerson.ca>
2020-11-13 04:58:28 -0500

    added open in new tab to all links

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -27,11 +27,11 @@ const resume = props => (
         </ul>
         <h1>Awards</h1>
         <ul className="awards">
-            <li><a href="https://www.ryerson.ca/engineering-architectural-science/current-undergraduate/honours-recognition/">
+            <li><a href="https://www.ryerson.ca/engineering-architectural-science/current-undergraduate/honours-recognition/" target="_blank">
                 Dean's List Distinction</a></li>
-            <li><a href="https://www.ryerson.ca/graduate/future-students/financing-your-studies/scholarships-awards/ryerson-graduate-fellowships/">
+            <li><a href="https://www.ryerson.ca/graduate/future-students/financing-your-studies/scholarships-awards/ryerson-graduate-fellowships/" target="_blank"> 
                 Ryerson Graduate Fellowship Award</a></li>
-            <li><a href="https://www.mitacs.ca/en/programs/accelerate">
+            <li><a href="https://www.mitacs.ca/en/programs/accelerate" target="_blank">
                 Mitacs Accelerate Grant</a></li>
         </ul>
         <h1>Experience</h1>
@@ -39,24 +39,24 @@ const resume = props => (
         <p className="subtitle">Paid internship position funded by the Mitacs Accelerate program in partnership with Dr. Paul Babyn.</p> 
             <p>In this role I developed a deep learning solution used to reduce patient's exposure to radiation during CT 
             examinations. The proposal abstract can be found on the  
-            <a href="https://www.mitacs.ca/en/projects/low-dose-computed-tomography-denoising-using-deep-learning"> Mitacs website. </a>
-            This work was published in the IJCNN 2020 proceedings and the pre-print version is <a href="https://arxiv.org/abs/2006.14738">available online via arXiv. &nbps;
+            <a href="https://www.mitacs.ca/en/projects/low-dose-computed-tomography-denoising-using-deep-learning" target="_blank"> Mitacs website. </a>
+            This work was published in the IJCNN 2020 proceedings and the pre-print version is <a href="https://arxiv.org/abs/2006.14738" target="_blank">available online via arXiv. &nbps;
             </a>
-            The source code for the project is available on <a href="https://github.com/s2ataei/research">GitHub</a> 
+            The source code for the project is available on <a href="https://github.com/s2ataei/research" target="_blank">GitHub</a> 
             </p>
         <h3 className="margin">Graduate Assistant: 09/2018 - 07/2020</h3>
         <p className="subtitle">Paid teaching assistant position at Ryerson University</p>
         <p>In this role I lead labs and tutorials for undergraduate engineering courses in Java, signal processing and image processing. 
             I was responsible for invigilating and grading examinations and preparing lessons for tutorials. </p>
         <h3 className="margin">Software Engineer: 05/2018 - 09/2018 </h3>
-        <p>Full-time role at <a href="https://wolf.ca">WOLF Advanced Technology. </a> </p>
+        <p>Full-time role at <a href="https://wolf.ca" target="_blank">WOLF Advanced Technology. </a> </p>
         <p>
             In this role I built business tools using Visual Basic and developed MRP (materials requirement planning) reports using SQL queries.
             I improved the efficiency of the MRP process by developing software tools to automate inventory analysis and procurement. My contributions were tools used for parsing Bill of Materials
             (BOM) data and calculating a list of parts to procure by reconciling available inventory. 
         </p>
         <h3 className="margin">Business Development Engineer: 06/2017 - 05/2018 </h3>
-        <p>Full-time role at <a href="https://wolf.ca">WOLF Advanced Technology. </a> </p>
+        <p>Full-time role at <a href="https://wolf.ca" target="_blank">WOLF Advanced Technology. </a> </p>
         <p>
             In this role I was responsible for handling sales opportunities and managing orders from the lead stage to the delivery stage. 
             I contributed to 70% year over year growth in sales by writing proposals and quotations and working to improve on-time delivery.
--- src/components/UserProfile/UserProfile.js
+++ src/components/UserProfile/UserProfile.js
@@ -14,7 +14,7 @@ const userprofile = props => (
             <p>I'm passionate about building scalable software solutions to real world problems.</p>
             <p>During my graduate research I developed deep learning solutions to medical imaging problems and published my work in the IJCNN 2020 proceedings.</p>
             <p>
-            Check out the <a href="https://arxiv.org/abs/2006.14738">pre-print on arXiv</a> and the <a href="https://github.com/s2ataei/research">code</a>.
+            Check out the <a href="https://arxiv.org/abs/2006.14738" target="_blank">pre-print on arXiv</a> and the <a href="https://github.com/s2ataei/research" target="_blank">code</a>.
             </p>
             <p>Want to get in touch? Please email me:
                 <a href="mailto:sepehr.ataei222@gmail.com"> sepehr.ataei222@gmail.com
